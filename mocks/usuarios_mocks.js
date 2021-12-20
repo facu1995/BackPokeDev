1536fd37c4c68203b61d4fbc8842f52c476605cc
Facundo <facuunion13@gmail.com>
2021-12-20 15:52:21 -0300

    contratar pokemon

--- mocks/usuarios_mocks.js
+++ mocks/usuarios_mocks.js
@@ -1,7 +1,7 @@
 const userData = [
-    { "email": "foo@foo.com", "name": "Ash", "pass": "foo123" },
-    { "email": "bar@bar.com", "name": "Misty", "pass": "bar123" },
-    { "email": "qux@qux.com", "name": "Brock", "pass": "qux123" },
+    { "email": "foo@foo.com", "name": "Ash", "pass": "foo123", "pokemons":[25,1] },
+    { "email": "bar@bar.com", "name": "Misty", "pass": "bar123","pokemons":[2] },
+    { "email": "qux@qux.com", "name": "Brock", "pass": "qux123","pokemons":[95] },
 ];
 
 module.exports= {
